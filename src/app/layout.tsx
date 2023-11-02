65511c75cc4b7270b713ab5827c5e599e60dcc61
Anthony Wallace <anthonyt.wallace@icloud.com>
2023-11-02 12:26:36 -0700

    Initial portfolio

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Anthony Wallace',
+    default: 'Anthony Wallace - Software Engineer',
+  },
+  description: 'I’m Anthony, a software engineer based in San Diego, CA.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
